86a159a4c8ae9ba37541b13a3fc5c07923d288eb
Luiz Henrique Buris <henrique.buris@outlook.com.br>
2024-01-15 14:31:42 -0300

    Add files via upload

---
+++ train.py
@@ -0,0 +1,215 @@
+#Buris L.H
+
+import os
+import torch
+import torchvision
+from torchvision import datasets, models
+from torchvision.transforms import functional as FT
+from torchvision import transforms as T
+from torch import nn, optim
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, sampler, random_split, Dataset
+import copy
+import math
+from PIL import Image
+import cv2
+import albumentations as A  # our data augmentation library
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import matplotlib.pyplot as plt
+
+import argparse
+import sys
+
+# remove arnings (optional)
+import warnings
+warnings.filterwarnings("ignore")
+from collections import defaultdict, deque
+import datetime
+import time
+from tqdm import tqdm # progress bar
+from torchvision.utils import draw_bounding_boxes
+
+# our dataset is in cocoformat, we will need pypcoco tools
+#!pip install pycocotools
+from pycocotools.coco import COCO
+
+# Now, we will define our transforms
+from albumentations.pytorch import ToTensorV2
+
+parser = argparse.ArgumentParser(description='PyTorch Pré_Treined fasterrcnn')
+parser.add_argument("--dataset_path", default="", type=str, required=True,
+                    help="informação.")
+parser.add_argument('--batch_size',
+                    default=4, type=int, help='batch_size')
+parser.add_argument('--epoch', default=100,
+                    type=int, help='you need in the epoch')
+#parser.add_argument('--num-workers', type=int,
+ #                   default=4, help='number of workers')
+parser.add_argument("--input_size", default=600,
+                    type=int, help="input size img.")
+
+
+args = parser.parse_args()
+
+
+dataset_path = "/content/placas/"
+seed = '15012024'
+
+def get_transforms(train=False):
+    if train:
+        transform = A.Compose([
+            A.Resize(args.input_size, args.input_size), # our input size can be 600px
+            A.HorizontalFlip(p=0.3),
+            A.VerticalFlip(p=0.3),
+            A.RandomBrightnessContrast(p=0.1),
+            A.ColorJitter(p=0.1),
+            ToTensorV2()
+        ], bbox_params=A.BboxParams(format='coco'))
+    else:
+        transform = A.Compose([
+            A.Resize(args.input_size, args.input_size), # our input size can be 600px
+            ToTensorV2()
+        ], bbox_params=A.BboxParams(format='coco'))
+    return transform
+
+class Dataset_Detection(datasets.VisionDataset):
+    def __init__(self, root, split='train', transform=None, target_transform=None, transforms=None):
+        # the 3 transform parameters are reuqired for datasets.VisionDataset
+        super().__init__(root, transforms, transform, target_transform)
+        self.split = split #train, valid, test
+        self.coco = COCO(os.path.join(root, split, "_annotations.coco.json")) # annotatiosn stored here
+        self.ids = list(sorted(self.coco.imgs.keys()))
+        self.ids = [id for id in self.ids if (len(self._load_target(id)) > 0)]
+
+    def _load_image(self, id: int):
+        path = self.coco.loadImgs(id)[0]['file_name']
+        image = cv2.imread(os.path.join(self.root, self.split, path))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
+    def _load_target(self, id):
+        return self.coco.loadAnns(self.coco.getAnnIds(id))
+
+    def __getitem__(self, index):
+        id = self.ids[index]
+        image = self._load_image(id)
+        target = self._load_target(id)
+        target = copy.deepcopy(self._load_target(id))
+
+        boxes = [t['bbox'] + [t['category_id']] for t in target] # required annotation format for albumentations
+        if self.transforms is not None:
+            transformed = self.transforms(image=image, bboxes=boxes)
+
+        image = transformed['image']
+        boxes = transformed['bboxes']
+
+        new_boxes = [] # convert from xywh to xyxy
+        for box in boxes:
+            xmin = box[0]
+            xmax = xmin + box[2]
+            ymin = box[1]
+            ymax = ymin + box[3]
+            new_boxes.append([xmin, ymin, xmax, ymax])
+
+        boxes = torch.tensor(new_boxes, dtype=torch.float32)
+
+        targ = {} # here is our transformed target
+        targ['boxes'] = boxes
+        targ['labels'] = torch.tensor([t['category_id'] for t in target], dtype=torch.int64)
+        targ['image_id'] = torch.tensor([t['image_id'] for t in target])
+        targ['area'] = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0]) # we have a different area
+        targ['iscrowd'] = torch.tensor([t['iscrowd'] for t in target], dtype=torch.int64)
+        return image.div(255), targ # scale images
+    def __len__(self):
+        return len(self.ids)
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+def save_model(model):
+    torch.save(model.state_dict(), args.dataset_path + "/"+str(seed)+"custon_fasterrcnn_mobilenet_v3_large_fpn.pth")
+
+### train
+def train_one_epoch(model, optimizer, loader, device, epoch):
+    model.to(device)
+    model.train()
+
+#     lr_scheduler = None
+#     if epoch == 0:
+#         warmup_factor = 1.0 / 1000 # do lr warmup
+#         warmup_iters = min(1000, len(loader) - 1)
+
+#         lr_scheduler = optim.lr_scheduler.LinearLR(optimizer, start_factor = warmup_factor, total_iters=warmup_iters)
+
+    all_losses = []
+    all_losses_dict = []
+
+    for images, targets in tqdm(loader):
+        images = list(image.to(device) for image in images)
+        targets = [{k: torch.tensor(v).to(device) for k, v in t.items()} for t in targets]
+
+        loss_dict = model(images, targets) # the model computes the loss automatically if we pass in targets
+        losses = sum(loss for loss in loss_dict.values())
+        loss_dict_append = {k: v.item() for k, v in loss_dict.items()}
+        loss_value = losses.item()
+
+        all_losses.append(loss_value)
+        all_losses_dict.append(loss_dict_append)
+
+        if not math.isfinite(loss_value):
+            print(f"Loss is {loss_value}, stopping trainig") # train if loss becomes infinity
+            print(loss_dict)
+            sys.exit(1)
+
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+
+#         if lr_scheduler is not None:
+#             lr_scheduler.step() #
+
+    all_losses_dict = pd.DataFrame(all_losses_dict) # for printing
+    print("Epoch {}, lr: {:.6f}, loss: {:.6f}, loss_classifier: {:.6f}, loss_box: {:.6f}, loss_rpn_box: {:.6f}, loss_object: {:.6f}".format(
+        epoch, optimizer.param_groups[0]['lr'], np.mean(all_losses),
+        all_losses_dict['loss_classifier'].mean(),
+        all_losses_dict['loss_box_reg'].mean(),
+        all_losses_dict['loss_rpn_box_reg'].mean(),
+        all_losses_dict['loss_objectness'].mean()
+    ))
+
+def main():
+ 
+  #load classes
+  coco = COCO(os.path.join(args.dataset_path, "train", "_annotations.coco.json"))
+  categories = coco.cats
+  n_classes = len(categories.keys())
+  print(categories)
+
+  classes = [i[1]['name'] for i in categories.items()]
+  print(classes)
+
+  train_dataset = Dataset_Detection(root=args.dataset_path, transforms=get_transforms(True))
+  train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4, collate_fn=collate_fn)
+
+  # lets load the faster rcnn model
+  model = models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True)
+  in_features = model.roi_heads.box_predictor.cls_score.in_features # we need to change the head
+  model.roi_heads.box_predictor = models.detection.faster_rcnn.FastRCNNPredictor(in_features, n_classes)
+
+  device = torch.device("cpu") # use GPU to train
+
+  model = model.to(device)
+
+  # Now, and optimizer
+  params = [p for p in model.parameters() if p.requires_grad]
+  optimizer = torch.optim.SGD(params, lr=0.01, momentum=0.9, nesterov=True, weight_decay=1e-4)
+
+  for epoch in range(args.epoch):
+    train_one_epoch(model, optimizer, train_loader, device, epoch)
+
+  save_model(model)
+ 
+
+if __name__ == '__main__':
+    main()
